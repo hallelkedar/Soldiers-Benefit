0963946e18655dfb24623e8beb5d1cf2f7c48b70
Hallel <hallelkedar000@gmail.com>
2026-07-29 12:20:38 +0300

    Added mock repos for budget and spends

---
+++ mock/spendsMockRepo.js
@@ -0,0 +1,24 @@
+import { mock } from "node:test";
+
+const spends = [
+    {
+        id: 1,
+        budgetId: 12,
+        amount: 30,
+        reason: 'Be',
+    }
+]
+
+export default {
+    create: mock.fn(async (data) => {
+        const existingIds = spends.map(spend => spend.id)
+        const newId = Math.max(1, ...existingIds) + 1
+        const createdAt = new Date().toDateString()
+        const spend = {newId, createdAt, ...data}
+        spends.push(spend)
+        return spend
+    }),
+    getAll: mock.fn(async () => {
+        return spends
+    })
+}
