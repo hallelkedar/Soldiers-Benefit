3af433cae76c185a9478e5afa009a1b43f393430
Hallel <hallelkedar000@gmail.com>
2026-07-29 17:42:36 +0300

    Added service tests skeleton

--- test/service.test/budgetService.test.js
+++ test/service.test/budgetService.test.js
@@ -8,14 +8,15 @@ const budgetService = createBudgetService(budgetMockRepo);
 describe("Budget service test", () => {
   describe("createBudget test", () => {
     it("Should return the new budget as it's saved", async () => {
-      const budget = {
-        unit: "8200",
-        benefitType: "giftCard",
-        month: "2026-10",
-        allocatedAmount: 16,
-      };
-      const result = await budgetService.createBudget(budget)
-      assert.deepEqual(result, {})
+      
     });
+    it("Should throw error because unit+benefitType+month is already exists", async () => {
+
+    })
   });
+  describe("getBudgets test", () => {
+    it("Should return array of budget with that unit", async () => {
+
+    })
+  })
 });
---
+++ test/service.test/spendsService.test.js
@@ -0,0 +1,31 @@
+import { it, describe } from "node:test";
+import assert from "node:assert";
+import createSpendsService from "../../service/spendsService.js";
+import spendsMockRepo from "../../mock/spendsMockRepo.js";
+
+const spendsService = createSpendsService(spendsMockRepo);
+
+describe("Spends service test", () => {
+  describe("createSpend test", () => {
+    it("Should return the new spends and remainingAmount", async () => {
+      
+    });
+    it("Should return failed message and remainingAmount", async () => {
+
+    })
+    it("Should throw error - budget not found", async () => {
+
+    })
+  });
+  describe("getTransactions test", () => {
+    it("Should return all the transactions of budgetId", async () => {
+
+    })
+    it("Should throw error - budget not found", async () => {
+
+    })
+    it("Should return empty array for budget with no attached transactions", async () => {
+        
+    })
+  })
+});
