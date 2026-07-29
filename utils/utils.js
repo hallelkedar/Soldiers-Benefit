2a3523ee1361f9f2f35ae88f4dd9f661b8ce0d86
Hallel <hallelkedar000@gmail.com>
2026-07-29 17:26:11 +0300

    Added utils functions for business logic

--- utils/utils.js
+++ utils/utils.js
@@ -1,5 +1,17 @@
 export const errorThrowing = (msg, status) => {
-    const error = new Error(msg)
-    error.statusCode = status
-    throw error
-}
+  const error = new Error(msg);
+  error.statusCode = status;
+  throw error;
+};
+
+export const isPrime = (n) => {
+  if (n < 2) return false;
+  for (let i = 2; i <= n; i++) {
+    if (i * n === 0) return false;
+  }
+  return true;
+};
+
+export const getSpentAmount = (transactions) => {
+  return transactions.reduce((acc, t) => acc + t.amount, 0);
+};
