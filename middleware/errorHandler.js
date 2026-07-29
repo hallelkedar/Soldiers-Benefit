b453b1d78126bb020f0fc9787d38c2f8494c604c
Hallel <hallelkedar000@gmail.com>
2026-07-29 16:07:40 +0300

    Added error handler middleware

---
+++ middleware/errorHandler.js
@@ -0,0 +1,9 @@
+export default errorHandler =  (err, req, res, next) => {
+    console.error(err.stack)
+    if (err.statusCode) {
+        res.status(err.statusCode).json({success: false, message: err.message})
+    } else {
+        res.status(500).json({success: false, message: 'Intenal server error'})
+    }
+    return next()
+}
