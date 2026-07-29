2e82cc3e5f704523d35c1d8ad92bab8066b5daca
Hallel <hallelkedar000@gmail.com>
2026-07-29 16:06:00 +0300

    Added spends service functions - get and create business logic, and update budget service logic

--- service/budgetService.js
+++ service/budgetService.js
@@ -5,7 +5,28 @@ export default function createBudgetService(repo) {
   return {
     createBudget: async (data) => {
       const { unit, benefitType, month, allocatedAmount } = data;
+      const match = await repo.find(unit, benefitType, month)
+      if (match) {
+        errorThrowing(`Exact combination is already exists ${match.id}`, 409)
+      }
+      const budget = await repo.create({
+        unit,
+        benefitType,
+        month,
+        allocatedAmount,
+      });
     },
+    getBudgets: async (filter) => {
+        const budgets = await repo.find(...filter)
+        return budgets.map(budget => {
+            const spentAmount = ''// TODO Calculate all it's transactions (spends)
+            return {
+                ...budget,
+                spentAmount, 
+                remainingAmount: budget.allocatedAmount - spentAmount
+            }
+    })
+        }
 
+    }
   };
-}
---
+++ service/spendsService.js
@@ -0,0 +1,33 @@
+import { errorThrowing } from "../utils/utils"
+
+export default function createSpendsService (spendsRepo, budgetRepo) {
+    return {
+        getTransactions: async (budgetId) => {
+            if (!await budgetRepo.getById(budgetId)) {
+                errorThrowing(`Budget not found`, 404)
+            }
+            const transactions = await spendsRepo.getByBudgetId(budgetId)
+            return transactions
+
+        },
+        createSpend: async (budgetId, amount, reason=null) => {
+            const budget = await budgetRepo.getById(budgetId)
+            if (!budget) {
+                errorThrowing(`Budget not found`, 404)
+            }
+            const transactions = await spendsRepo.getByBudgetId(budgetId)
+            const spentAmount = transactions.reduce((acc, t) => acc + t.amount, 0)
+            const remainingAmount = budget.allocatedAmount - spentAmount
+            if (spentAmount + amount > budget.allocatedAmount) {
+                return {error: 'Amount is more than allowed', remainingAmount}
+            }
+            
+            const data = reason ? {budgetId, amount, reason} : {budgetId, amount}
+            const transaction = await spendsRepo.create(data)
+            return {transaction, remainingAmount: remainingAmount + amount}
+        },
+        getSpentAmount: async (transactions) => {
+            return transactions.reduce((acc, t) => acc + t.amount, 0)
+        }
+    }
+}
